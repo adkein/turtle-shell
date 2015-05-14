33e6aed0d50726eaf1c674e8acfd6de55b9202f0
Carl Smith <carl.smith@moat.com>
2015-05-14 04:09:26 -0400

    ipython profile configs

--- config/ipython/profile_default/static/custom/custom.js
+++ config/ipython/profile_default/static/custom/custom.js
@@ -47,3 +47,18 @@
  * @class customjs
  * @static
  */
+
+code_show=true;
+function code_toggle() {
+ if (code_show){
+ $('div.input').hide();
+ } else {
+ $('div.input').show();
+ }
+ code_show = !code_show
+}
+
+$([IPython.events]).on("app_initialized.NotebookApp", function () {
+  IPython.load_extensions('IPython-notebook-extensions/usability/codefolding/codefolding');
+  $("#view_menu").append("<li id=\"toggle_toolbar\" title=\"Show/Hide code cells\"><a href=\"javascript:code_toggle()\">Toggle Code Cells</a></li>");
+});
